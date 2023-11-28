9963e6104d75d931a0383909af61fc4d13f63a52
Gurjant Singh <gurjantrandhawa74456@gmail.com>
2023-11-28 20:58:52 +0530

    Next js 14 anime webapp

--- app/actions.ts
+++
@@ -1,9 +0,0 @@
-"use server";
-
-export const fetchAnime = async () => {
-    const response = await fetch("https://shikimori.one/api/animes");
-    const data = await response.json();
-    console.log(data);
-
-    return data;
-}
